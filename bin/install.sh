778d8b63e3033fa2519f0894e18d966259cbb548
David Barri <japgolly@gmail.com>
2012-03-21 09:50:36 +1100

    Added a --force option to the install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$1" == "-f" -o "$1" == "--force" ]; then
+  force=1
+  shift
+fi
+
 old_dir="$(pwd)"
 cd "$(dirname $0)/../assets/" && ASSETS="$(pwd)" && cd "$old_dir"
 [ ! -e "$ASSETS" ] && echo "Failed to determine asset directory." && exit 2
@@ -13,9 +18,11 @@ function link {
   if [ ! -e "$s" ]; then
     ln -s "$t" "$s" && echo "ok" || echo "failed"
   elif [ -L "$s" ]; then
-    rm "$s" && ln -s "$t" "$s" && echo "replaced link" || echo "failed"
+    rm "$s" && ln -s "$t" "$s" && echo "replaced link, ok" || echo "failed"
   elif [ -f "$s" -a ! -s "$s" ]; then
-    rm "$s" && ln -s "$t" "$s" && echo "replaced empty file" || echo "failed"
+    rm "$s" && ln -s "$t" "$s" && echo "replaced empty file, ok" || echo "failed"
+  elif [ -n "$force" ]; then
+    rm -f "$s" && ln -s "$t" "$s" && echo "replaced existing file, ok" || echo "failed"
   else
     echo "already exists, aborting"
   fi
