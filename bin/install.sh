ea5621c45f1142653c4e0c212f10f65e26b243aa
David Barri <japgolly@gmail.com>
2012-03-21 10:36:39 +1100

    Now updates .bashrc properly.

--- bin/install.sh
+++ bin/install.sh
@@ -5,6 +5,8 @@ if [ "$1" == "-f" -o "$1" == "--force" ]; then
   shift
 fi
 
+# ----------------------------------------------------------------------------------------------------------------------
+
 old_dir="$(pwd)"
 cd "$(dirname $0)/../assets/" && ASSETS="$(pwd)" && cd "$old_dir"
 [ ! -e "$ASSETS" ] && echo "Failed to determine asset directory." && exit 2
@@ -13,7 +15,7 @@ function link {
   printf "Symlink: %s ... " "$1"
   s="$HOME/$1"
   t="$ASSETS/$1"
-  [ ! -e "$t" ] && echo "Asset not found: $t" && exit 3
+  [ ! -e "$t" ] && echo -e "aborting\nAsset not found: $t" && exit 3
 
   if [ ! -e "$s" ]; then
     ln -s "$t" "$s" && echo "ok" || echo "failed"
@@ -28,12 +30,32 @@ function link {
   fi
 }
 
+function source_script {
+  t="$2"; [ -z "$t" ] && t="$1"
+  printf "Source: %s <- %s ... " "$1" "$t"
+  s="$HOME/$1"
+  t="$ASSETS/$t"
+  [ ! -e "$t" ] && echo -e "aborting\nAsset not found: $t" && exit 3
+
+  cmd='[ -r "'"$t"'" ] && source "'"$t"'"'
+  tmp=/tmp/golly-rc.$$.tmp
+  (cat "$s" | fgrep -v "$cmd" && echo "$cmd") > $tmp
+  if [ $? -ne 0 ]; then
+    echo "failed"
+  else
+    cat $tmp > "$s" && echo "updated, ok" || echo "failed"
+  fi
+}
+
+# ----------------------------------------------------------------------------------------------------------------------
+
 link .aprc
 link .gitconfig
 link .irbrc
 link .vimrc
 
-echo "Hardcoded appending to bashrc (TODO)"
-s=~/.bashrc
-t="$ASSETS"/.bashrc
-echo '[ -r "'"$t"'" ] && source "'"$t"'"' >> "$s"
+source_script .bashrc
+case "$(uname -a)" in
+  *-ARCH[^A-Za-z]*) source_script .bashrc .bashrc-arch ;;
+  # TODO ubuntu
+esac
