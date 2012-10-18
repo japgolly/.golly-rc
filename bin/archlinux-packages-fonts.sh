745637bfd1274310476b89e172cf879bc8d061a8
David Barri <japgolly@gmail.com>
2012-10-18 13:37:41 +1100

    Added bin/archlinux-packages-fonts.sh

---
+++ bin/archlinux-packages-fonts.sh
@@ -0,0 +1,6 @@
+ yaourt -Sy --needed --noconfirm -- \
+   ttf-arphic-uming ttf-bitstream-vera ttf-linux-libertine ttf-mph-2b-damase \
+   terminus-font-ttf ttf-anonymous-pro ttf-adobe-fonts ttf-chromeos-fonts \
+   ttf-envy-code-r ttf-inconsolata-g ttf-kochi-substitute ttf-meguri \
+   ttf-microsoft-consolas \
+   otf-ipafont ttf-vlgothic ttf-ipa-mona ttf-monapo
