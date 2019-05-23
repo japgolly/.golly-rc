85f40a4b96cf0e53ec2be5a8b30372dcd09dd9c6
David Barri <japgolly@gmail.com>
2019-05-24 09:13:30 +1000

    Highlight bin/install failures

--- bin/install.sh
+++ bin/install.sh
@@ -7,6 +7,10 @@ fi
 
 # ----------------------------------------------------------------------------------------------------------------------
 
+function echoF {
+  echo -e "\e[1;31m$1\e[0m"
+}
+
 old_dir="$(pwd)"
 cd "$(dirname "$0")/../assets/." && ASSETS="$(pwd)" && cd "$old_dir"
 [ ! -e "$ASSETS" ] && echo "Failed to determine asset directory." && exit 2
@@ -16,18 +20,18 @@ function link {
   printf "Symlink: %s <- %s ... " "$1" "$t"
   s="$HOME/$1"
   t="$ASSETS/$t"
-  [ ! -e "$t" ] && echo -e "aborting\nAsset not found: $t" && exit 3
+  [ ! -e "$t" ] && echoF "aborting\nAsset not found: $t" && exit 3
 
   if [ ! -e "$s" ]; then
-    ln -s "$t" "$s" && echo "ok" || echo "failed"
+    ln -s "$t" "$s" && echo "ok" || echoF "failed"
   elif [ -L "$s" ]; then
-    rm "$s" && ln -s "$t" "$s" && echo "replaced link, ok" || echo "failed"
+    rm "$s" && ln -s "$t" "$s" && echo "replaced link, ok" || echoF "failed"
   elif [ -f "$s" -a ! -s "$s" ]; then
-    rm "$s" && ln -s "$t" "$s" && echo "replaced empty file, ok" || echo "failed"
+    rm "$s" && ln -s "$t" "$s" && echo "replaced empty file, ok" || echoF "failed"
   elif [ -n "$force" ]; then
-    rm -f "$s" && ln -s "$t" "$s" && echo "replaced existing file, ok" || echo "failed"
+    rm -f "$s" && ln -s "$t" "$s" && echo "replaced existing file, ok" || echoF "failed"
   else
-    echo "already exists, aborting"
+    echoF "already exists, aborting"
   fi
 }
 
@@ -36,13 +40,13 @@ function source_script {
   printf "Source: %s <- %s ... " "$1" "$t"
   s="$HOME/$1"
   t="$ASSETS/$t"
-  [ ! -e "$t" ] && echo -e "aborting\nAsset not found: $t" && exit 3
+  [ ! -e "$t" ] && echoF "aborting\nAsset not found: $t" && exit 3
 
   cmd='[ -r "'"$t"'" ] && source "'"$t"'"'
   if [ ! -e "$s" ]; then
     echo "$cmd" >> "$s"
     if [ ! -e $tmp ]; then
-      echo "failed"
+      echoF "failed"
     else
       echo "created, ok"
     fi
@@ -50,9 +54,9 @@ function source_script {
     tmp=/tmp/golly-rc.$$.tmp
     (cat "$s" | (fgrep -v "$cmd" || true) && echo "$cmd") > $tmp
     if [ ! -e $tmp ]; then
-      echo "failed"
+      echoF "failed"
     else
-      cat $tmp > "$s" && echo "updated, ok" || echo "failed"
+      cat $tmp > "$s" && echo "updated, ok" || echoF "failed"
     fi
   fi
 }
